9967dba4d546b9944c38057b28234071d6e578c7
Marton Lederer <marton@lederer.hu>
2023-01-07 10:20:59 +0100

    feat: strategies wip

--- src/strategies/Strategy.ts
+++ src/strategies/Strategy.ts
@@ -1,3 +1,5 @@
+import { PermissionType, AppInfo, GatewayConfig } from "arconnect";
+
 export default abstract class Strategy {
   // info
   public abstract name: string;
@@ -6,11 +8,16 @@ export default abstract class Strategy {
   public abstract logo: string;
 
   // connection
-  public abstract connect(): Promise<void>;
+  public abstract connect(
+    permissions: PermissionType[],
+    appInfo?: AppInfo,
+    gateway?: GatewayConfig
+  ): Promise<void>;
   public abstract disconnect(): Promise<void>;
 
   // other apis
-  public abstract getActiveAddress(): Promise<string>;
+  /*public abstract getActiveAddress(): Promise<string>;
   public abstract getAllAddresses(): Promise<string[]>;
   public abstract sign(): Promise<void>;
+  public abstract getPermissions(): Promise<PermissionType[]>;*/
 }
---
+++ src/strategies/index.ts
@@ -0,0 +1,8 @@
+import ArConnectStrategy from "./ArConnect"
+import Strategy from "./Strategy"
+
+const strategies: Strategy[] = [
+  new ArConnectStrategy()
+];
+
+export default strategies;
