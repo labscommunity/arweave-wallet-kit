d3a85503e5a9b45a9d0e245749ca74e2a467420d
Marton Lederer <marton@lederer.hu>
2023-01-07 21:16:50 +0100

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as useActiveAddress } from "./hooks/active_address";
+export { default as useActiveAddress, usePublicKey } from "./hooks/active_address";
+export { default as useAddresses, useWalletNames } from "./hooks/addresses";
 export { default as useConnection } from "./hooks/connection";
 export { ArConnectKit } from "./components/Provider";
 export { useStrategy } from "./hooks/strategy";
