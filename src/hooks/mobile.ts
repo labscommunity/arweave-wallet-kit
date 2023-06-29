aa3a689c4c5a5012778ba530027b41aacc9fd252
Marton Lederer <marton@lederer.hu>
2023-06-29 08:27:03 +0200

    fix: useMobile not working in nextjs

--- src/hooks/mobile.ts
+++ src/hooks/mobile.ts
@@ -6,11 +6,17 @@ import { useEffect, useMemo, useState } from "react";
 export default function useMobile(mobileWidth = 720) {
   const [mobile, setMobile] = useState(false);
   const query = useMemo(
-    () => window.matchMedia(`(max-width: ${mobileWidth}px)`),
+    () => {
+      if (typeof window === "undefined") return null;
+
+      return window.matchMedia(`(max-width: ${mobileWidth}px)`);
+    },
     [mobileWidth]
   );
 
   useEffect(() => {
+    if (!query) return;
+
     const listener = (e: MediaQueryListEvent) => setMobile(e.matches);
 
     query.addEventListener("change", listener);
