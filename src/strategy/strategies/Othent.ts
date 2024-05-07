98c6f8ca5ae35ee0836fe760f02391104ff61543
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-05-07 12:54:20 +0100

    feat: update othent strategy

--- src/strategy/strategies/Othent.ts
+++ src/strategy/strategies/Othent.ts
@@ -1,314 +1,126 @@
+import * as othentKMS from '@othent/kms'
+import type { ConnectReturnType } from '@othent/kms'
+import type Transaction from "arweave/node/lib/transaction";
 import type { SignatureOptions } from "arweave/node/lib/crypto/crypto-interface";
-import type { DispatchResult, GatewayConfig, PermissionType } from "arconnect";
-import type Transaction from "arweave/web/lib/transaction";
-import type { AppInfo } from "arweave-wallet-connector";
-import type Strategy from "../Strategy";
-import { decodeTags } from "../../utils";
-import {
-  Othent,
-  queryWalletAddressTxnsProps,
-  readCustomContractProps,
-  SendTransactionArweaveProps,
-  SendTransactionBundlrProps,
-  SendTransactionWarpProps,
-  SignTransactionBundlrProps,
-  SignTransactionWarpProps,
-  useOthentReturnProps,
-  verifyArweaveDataProps,
-  verifyBundlrDataProps
-} from "othent";
+import type {
+  PermissionType,
+  AppInfo,
+  GatewayConfig,
+} from "arconnect";
 
-export default class OthentStrategy implements Strategy {
+export default class OthentStrategy
+{
   public id: "othent" = "othent";
   public name = "Google";
   public description =
-    "Sign in with Google through Othent Smart Contract Wallets";
+    "Sign in with Google through Othent 2.0";
   public theme = "35, 117, 239";
   public logo = "33nBIUNlGK4MnWtJZQy9EzkVJaAd7WoydIKfkJoMvDs";
   public url = "https://othent.io";
 
-  #apiID = "e923634af8cc8b63bc8c74735d177aae";
-  #addressListeners: ListenerFunction[] = [];
-  #othent: useOthentReturnProps | undefined;
-  #currentAddress: string = "";
-
   constructor() {}
 
-  /**
-   * Advanced function to override the default API ID
-   * Othent uses.
-   */
-  public __overrideApiID(othentApiID: string) {
-    this.#apiID = othentApiID;
-  }
-
-  async #othentInstance(ensureConnection = true) {
-    if (this.#othent && !ensureConnection) {
-      return this.#othent;
-    }
-
-    if (!this.#othent) {
-      try {
-        // init othent
-        this.#othent = await Othent({
-          API_ID: this.#apiID
-        });
-      } catch (error: any) {
-        throw new Error(`[Arweave Wallet Kit] ${error.message ?? error}`);
-      }
-    }
-
-    if (ensureConnection) {
-      const permissions = await this.getPermissions();
-
-      if (permissions.length === 0) {
-        throw new Error("[Arweave Wallet Kit] You are not connected to Othent");
-      }
-    }
-
-    return this.#othent;
-  }
-
   public async isAvailable() {
-    try {
-      // ensure instance
-      await this.#othentInstance(false);
-
+    if (othentKMS) {
       return true;
-    } catch {
-      return false;
     }
   }
 
-  public async ping() {
-    const othent = await this.#othentInstance(false);
+  public async sync() {}
 
-    return await othent.ping();
-  }
-
-  public async connect(
-    permissions: PermissionType[],
+  public async connect(permissions: PermissionType[],
     appInfo?: AppInfo,
-    gateway?: GatewayConfig
-  ) {
-    const othent = await this.#othentInstance(false);
-    const res = await othent.logIn({ testNet: false });
-
-    for (const listener of this.#addressListeners) {
-      listener(res.contract_id);
-    }
-
-    this.#currentAddress = res.contract_id;
-  }
-
-  public async disconnect() {
-    const othent = await this.#othentInstance();
-
-    await othent.logOut();
-
-    for (const listener of this.#addressListeners) {
-      listener(undefined as any);
-    }
-
-    this.#currentAddress = "";
-  }
-
-  public async getPermissions() {
-    const othent = await this.#othentInstance(false);
-
-    try {
-      const res = await othent.userDetails();
-
-      if (res.contract_id !== this.#currentAddress) {
-        this.#currentAddress = res.contract_id;
-        for (const listener of this.#addressListeners) {
-          listener(res.contract_id);
-        }
+    gateway?: GatewayConfig): Promise<ConnectReturnType> {
+      if (permissions || appInfo || gateway) {
+        throw new Error("Permissions/appInfo/gateway, is not implemented in Othent");
       }
+    return await othentKMS.connect()
+  }
 
-      return [
-        "ACCESS_ADDRESS",
-        "ACCESS_PUBLIC_KEY",
-        "ACCESS_ALL_ADDRESSES",
-        "SIGN_TRANSACTION",
-        "ENCRYPT",
-        "DECRYPT",
-        "SIGNATURE",
-        "ACCESS_ARWEAVE_CONFIG",
-        "DISPATCH"
-      ] as PermissionType[];
-    } catch {
-      return [];
-    }
+  public async disconnect(): Promise<null> {
+    return await othentKMS.disconnect()
   }
 
   public async getActiveAddress(): Promise<string> {
-    const othent = await this.#othentInstance(false);
-    const res = await othent.userDetails();
-
-    return res.contract_id;
+    return await othentKMS.getActiveAddress()
   }
 
   public async getAllAddresses(): Promise<string[]> {
-    const addr = await this.getActiveAddress();
-
-    return [addr];
+    return [await othentKMS.getActiveAddress()]
   }
 
-  /**
-   * Same as "sendTransactionArweave()" of the Othent SDK
-   */
-  // @ts-expect-error - Return type is different for Othent transaction signing
-  public async sign(transaction: Transaction, options?: SignatureOptions) {
+  public async getWalletNames(): Promise<string> {
+    return await othentKMS.getWalletNames()
+  }
+
+  public async sign(
+    transaction: Transaction,
+    options?: SignatureOptions,
+    analyticsTags?: { name: string; value: string; }[] | undefined
+  ): Promise<any> {
+
     if (options) {
-      console.warn(
-        "[Arweave Wallet Kit] Othent does not support transaction signature options"
-      );
+      throw new Error("Options, are not implemented in Othent");
     }
 
-    if (transaction.quantity !== "0" && transaction.target !== "") {
-      throw new Error(
-        "[Arweave Wallet Kit] Signing with Othent only supports data type transactions"
-      );
+    return othentKMS.sign(transaction, analyticsTags);
+  }
+
+  public async encrypt(
+    data: string | Uint8Array,
+    algorithm?: RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams
+  ): Promise<string | Uint8Array | null> {
+    if (algorithm) {
+      throw new Error("Algorithm, is not implemented in Othent");
     }
-    const othent = await this.#othentInstance(false);
 
-    const signedTransaction = await othent.signTransactionArweave({
-      othentFunction: "uploadData",
-      data: transaction.data,
-      tags: decodeTags(transaction.tags)
-    });
-
-    return signedTransaction;
+    return await othentKMS.encrypt(data)
   }
 
-  public async signTransactionBundlr(params: SignTransactionBundlrProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.signTransactionBundlr(params);
-  }
-
-  public async signTransactionWarp(params: SignTransactionWarpProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.signTransactionWarp(params);
-  }
-
-  public async sendTransactionArweave(params: SendTransactionArweaveProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.sendTransactionArweave(params);
-  }
-
-  public async sendTransactionBundlr(params: SendTransactionBundlrProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.sendTransactionBundlr(params);
-  }
-
-  public async sendTransactionWarp(params: SendTransactionWarpProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.sendTransactionWarp(params);
-  }
-
-  public async verifyArweaveData(params: verifyArweaveDataProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.verifyArweaveData(params);
-  }
-
-  public async verifyBundlrData(params: verifyBundlrDataProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.verifyBundlrData(params);
-  }
-
-  public async dispatch(transaction: Transaction): Promise<DispatchResult> {
-    try {
-      const othent = await this.#othentInstance(false);
-
-      const tags = decodeTags(transaction.tags);
-
-      const signedTransaction = await othent.signTransactionBundlr({
-        othentFunction: "uploadData",
-        data: transaction.data,
-        tags
-      });
-
-      const res = await othent.sendTransactionBundlr(signedTransaction);
-
-      if (res.success === true) {
-        return { id: res.transactionId, type: "BUNDLED" };
-      } else {
-        try {
-          const othent = await this.#othentInstance(false);
-
-          const signedTransaction = await othent.signTransactionArweave({
-            othentFunction: "uploadData",
-            data: transaction.data,
-            tags
-          });
-
-          const res = await othent.sendTransactionArweave(signedTransaction);
-
-          if (res.success === true) {
-            return { id: res.transactionId, type: "BASE" };
-          } else {
-            throw new Error(
-              `Failed to dispatch layer transaction. Please recheck request.`
-            );
-          }
-        } catch (error) {
-          throw new Error(
-            `Unable to dispatch Base layer transaction: ${error}`
-          );
-        }
-      }
-    } catch (error) {
-      throw new Error(`Unable to dispatch Bundled transaction: ${error}`);
+  public async decrypt(
+    data: string | Uint8Array,
+    algorithm?: RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams
+  ): Promise<string | Uint8Array | null> {
+    if (algorithm) {
+      throw new Error("Algorithm, is not implemented in Othent");
     }
+    return await othentKMS.decrypt(data)
   }
 
-  public async queryWalletAddressTxns(params: queryWalletAddressTxnsProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.queryWalletAddressTxns(params);
+  public async getArweaveConfig(): Promise<Error> {
+    throw new Error("Not implemented in Othent");
   }
 
-  public async userDetails() {
-    const othent = await this.#othentInstance();
-
-    return await othent.userDetails();
+  public async signature(
+    data: string | Uint8Array | ArrayBuffer,
+    algorithm?: RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams
+  ): Promise<Uint8Array> {
+    if (algorithm) {
+      throw new Error("Algorithm, is not implemented in Othent");
+    }
+    return await othentKMS.signature(data)
   }
 
-  public async readContract() {
-    const othent = await this.#othentInstance();
-
-    return await othent.readContract();
+  public async getActivePublicKey(): Promise<string> {
+    return await othentKMS.getActivePublicKey()
   }
 
-  public async readCustomContract(params: readCustomContractProps) {
-    const othent = await this.#othentInstance();
-
-    return await othent.readCustomContract(params);
+  public async addToken(id: string): Promise<void> {
+    throw new Error("Not implemented in Othent");
   }
 
-  public addAddressEvent(listener: ListenerFunction) {
-    this.#addressListeners.push(listener);
+  public async dispatch(transaction: Transaction): Promise<{ id: string; }> {
+    return await othentKMS.dispatch(transaction)
+  }
 
-    // placeholder function
-    return listener as any;
+  public addAddressEvent(listener: (address: string) => void) {
+    throw new Error("Not implemented in Othent");
   }
 
   public removeAddressEvent(
     listener: (e: CustomEvent<{ address: string }>) => void
   ) {
-    this.#addressListeners.splice(
-      this.#addressListeners.indexOf(listener as any),
-      1
-    );
+    throw new Error("Not implemented in Othent");
   }
-}
 
-type ListenerFunction = (address: string) => void;
+}
