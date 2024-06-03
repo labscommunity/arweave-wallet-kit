22f4cfb7f2dd7fe7ebbc40e155768b64e052b0ce
7i7o <mparij@communitylabs.com>
2024-06-03 12:36:14 +0200

    update react example to latest vite

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,10 +1,10 @@
-import BrowserWalletStrategy from "@arweave-wallet-kit/browser-wallet-strategy";
-import { ArweaveWalletKit, ConnectButton } from "@arweave-wallet-kit/react";
 import ArConnectStrategy from "@arweave-wallet-kit/arconnect-strategy";
-import WebWalletStrategy from "@arweave-wallet-kit/webwallet-strategy";
+import BrowserWalletStrategy from "@arweave-wallet-kit/browser-wallet-strategy";
 import OthentStrategy from "@arweave-wallet-kit/othent-strategy";
+import { ArweaveWalletKit, ConnectButton } from "@arweave-wallet-kit/react";
+import WebWalletStrategy from "@arweave-wallet-kit/webwallet-strategy";
 
-export default function App() {
+function App() {
   return (
     <ArweaveWalletKit
       theme={{
@@ -12,37 +12,38 @@ export default function App() {
         accent: {
           r: 0,
           g: 0,
-          b: 0
+          b: 0,
         },
         titleHighlight: {
           r: 0,
           g: 122,
-          b: 255
+          b: 255,
         },
-        radius: "default"
+        radius: "default",
       }}
       config={{
         strategies: [
           new ArConnectStrategy(),
           new WebWalletStrategy(),
-          // @ts-expect-error
           new OthentStrategy(),
-          new BrowserWalletStrategy()
+          new BrowserWalletStrategy(),
         ],
         permissions: ["ACCESS_ADDRESS", "ACCESS_ALL_ADDRESSES"],
         ensurePermissions: true,
         appInfo: {
           name: "Test App",
-          logo: "https://arweave.net/tQUcL4wlNj_NED2VjUGUhfCTJ6pDN9P0e3CbnHo3vUE"
+          logo: "https://arweave.net/tQUcL4wlNj_NED2VjUGUhfCTJ6pDN9P0e3CbnHo3vUE",
         },
         gatewayConfig: {
           host: "arweave.net",
           port: 443,
-          protocol: "https"
-        }
+          protocol: "https",
+        },
       }}
     >
       <ConnectButton accent={"rgb(0, 122, 255)"} />
     </ArweaveWalletKit>
   );
 }
+
+export default App;
--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,9 +1,9 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
